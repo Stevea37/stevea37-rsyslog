83f9d7860cdf389a7bb9469cd170888cdca6439c
Steven Ayers <steveay99@gmail.com>
2017-03-21 12:26:26 +0000

    Adding repo for CentOS to install better version of Rsyslog

--- manifests/package.pp
+++ manifests/package.pp
@@ -5,25 +5,38 @@
 class rsyslog::package {
   notice("Running rsyslog::package")
 
-  if $::osfamily != "RedHat" {
-    include apt
-  }
+  if $::osfamily == "Debian" {
+    package { 'rsyslog':
+      name          => 'rsyslog',
+      ensure        => present,
+      allow_virtual => false
+    }
 
-
-
-  package { 'rsyslog':
-    name => 'rsyslog',
-    ensure  => present,
-    allow_virtual => false
-  }
-
-  if $osfamily != "RedHat" {
-    package { 'rsyslog-elasticsearch':
-      name          => 'rsyslog-elasticsearch',
+    package { 'rsyslog-relp':
+      name          => 'rsyslog-relp',
       ensure        => present,
       allow_virtual => false,
       require       => [
         Package['rsyslog'],
+      ],
+    }
+  }
+  elsif $::osfamily == "RedHat" {
+
+    file { '/etc/yum.repos.d/rsyslog.repo':
+      ensure  => present,
+      content => template('rsyslog/etc/rsyslog.repo.erb'),
+      owner   => 'root',
+      group   => 'root',
+    }
+
+    package { 'rsyslog':
+      name          => 'rsyslog',
+      ensure        => present,
+      allow_virtual => false,
+      install_options  => ['--disablerepo=amzn-main'],
+      require       => [
+        File['/etc/yum.repos.d/rsyslog.repo']
       ],
     }
 
@@ -31,6 +44,7 @@ class rsyslog::package {
       name          => 'rsyslog-relp',
       ensure        => present,
       allow_virtual => false,
+      install_options  => ['--disablerepo=amzn-main'],
       require       => [
         Package['rsyslog'],
       ],
